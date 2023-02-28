c1a9347f966105b6f068b6bae13d34fc57f804c8
Roberto Aranda Lopez <RobertoAranda133@gmail.com>
2023-02-28 19:45:15 +0100

    IC: Spring boot skeleton

---
+++ src/test/java/com/roberto/censoapi/CensoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.censoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
