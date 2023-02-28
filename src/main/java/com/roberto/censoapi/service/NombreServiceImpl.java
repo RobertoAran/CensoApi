4cf3a4221b062e97729a00b8c53582bda0c44283
Roberto Aranda Lopez <RobertoAranda133@gmail.com>
2023-02-28 20:37:13 +0100

    Dev. Añado a la implementacion metodos GET.

--- src/main/java/com/roberto/censoapi/service/NombreServiceImpl.java
+++ src/main/java/com/roberto/censoapi/service/NombreServiceImpl.java
@@ -1,4 +1,30 @@
 package com.roberto.censoapi.service;
 
-public class NombreServiceImpl {
+import com.roberto.censoapi.repository.NombreRepository;
+import com.roberto.censoapi.types.NombreType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NombreServiceImpl implements NombreService{
+
+    @Autowired
+    private NombreRepository nombreRepository;
+
+    @Override
+    public List<NombreType> findAll() {
+        List <NombreType> nombreType = nombreRepository.findAll();
+        return nombreType;
+    }
+
+    @Override
+    public NombreType getById(int idNombre) {
+        try{
+            return (NombreType) (nombreRepository.getNombreTypeById(idNombre));
+        }catch(Exception e){
+            return null; //Mejorar esta excepción
+        }
+    }
 }
