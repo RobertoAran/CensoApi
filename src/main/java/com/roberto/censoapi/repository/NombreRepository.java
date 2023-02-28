63227c092f283f1f4dec5177066df7a9f5b571a7
Roberto Aranda Lopez <RobertoAranda133@gmail.com>
2023-02-28 20:32:27 +0100

    Dev. Repositorio extend de jpa y principales GET.

--- src/main/java/com/roberto/censoapi/repository/NombreRepository.java
+++ src/main/java/com/roberto/censoapi/repository/NombreRepository.java
@@ -1,4 +1,16 @@
 package com.roberto.censoapi.repository;
 
-public interface NombreRepository {
+import com.roberto.censoapi.types.NombreType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NombreRepository extends JpaRepository<NombreType, Integer> {
+
+    NombreType getNombreTypeById (Integer id);
+
+    List<NombreType> getNombreTypeByNombre (String nombre);
+
+    @Override
+    List<NombreType> findAll();
 }
